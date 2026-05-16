1ad3dc4f3eed42f1e0aa520248b62c50128b82e0
Isaac Desmond <desmondisaac247@gmail.com>
2026-05-16 14:04:38 +0100

    correct line 13

--- bot.py
+++ bot.py
@@ -10,7 +10,8 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 # ── Config ────────────────────────────────────────────────────────────────────
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-NEWS_API_KEY = "db4858b5de9bfdfa000f06505d34df10"NEWS_API_URL = "https://gnews.io/api/v4"
+NEWS_API_KEY = "db4858b5de9bfdfa000f06505d34df10"
+NEWS_API_URL = "https://gnews.io/api/v4"
 
 logging.basicConfig(format="%(asctime)s | %(levelname)s | %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
